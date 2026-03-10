d28fcd02187c89f1233ccf8d36383bb460570df0
harvest7777 <333harvest.moon333@gmail.com>
2026-03-10 13:07:22 -0700

    chore: delete dead files

--- models.py
+++
@@ -1,7 +0,0 @@
-from uagents import Model
-
-# Notice this model is shared between Alice and Bob
-# This is because Bob expects this shaped message in his receive handler. So, we should
-# create messages with the expected shape
-class Message(Model):
-    content: str
